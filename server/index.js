1ccb155bd4c40871df3a83e31aa2bca6f6ea6615
Warrick FitzGerald <40346010+WarrickFitz@users.noreply.github.com>
2022-12-09 21:31:25 -0500

    missing

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+var cors = require('cors')
+const express = require('express');
+const dotenv = require('dotenv');
+var morgan = require('morgan');
+// const { addOreidExpressMiddleware } = require('oreid-js/dist/expressMiddleware');
+
+
+const PORT = 8080 // if you change this port, you should update "proxy" in package.json to match - "proxy" is used by React app during development only
+dotenv.config();
+const app = express();
+
+app.use(morgan('short')) // This logs requests to the proxy
+app.use(cors())
+
+// Disable caching so I can see the full request every time
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store') 
+    next()
+})
+
+function addOreidApiKeysMiddleware(options) {
+    return (req, _res, next) => {
+      // inject api-key to header of request
+      req.headers['api-key'] = options.apiKey // This just injects the apiKey into the header
+      next()
+    }
+}
+
+function onProxyReq(proxyReq, req, res) {
+    proxyReq.setHeader('host', process.env.REACT_APP_OREID_URL); // This is only really used when you're using a debug proxy
+}
+
+const proxyUrl = process.env.REACT_APP_PROXY_URL
+const target = proxyUrl ? proxyUrl : 'https://'+process.env.REACT_APP_OREID_URL
+
+console.log('target: ' + target)
+
+const addHeadersMiddleware = addOreidApiKeysMiddleware({ apiKey: process.env.SECRET_OREID_API_KEY })
+const frontendProxyRoute = createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: { 
+        '^/oreid': '' // Requests arrive /oreid/api/account/user?account=xx so we need to remove the /oreid
+    },
+    onProxyReq
+  })
+app.use('/oreid', addHeadersMiddleware, frontendProxyRoute)
+
+const listener = app.listen(PORT, () => {
+  console.log(`Express proxy server is running on port:${listener.address().port}`);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AuthProvider, ChainNetwork, OreId } from "oreid-js";
+import { WebPopup } from "oreid-webpopup";
+import { OreidProvider } from "oreid-react";
+
+
+
+let oreId : OreId 
+
+const appId = process.env.REACT_APP_OREID_APP_ID || ''
+const login = async () => {
+  console.log('login button clicked')
+
+  const oreIdUrl = 'https://'+process.env.REACT_APP_OREID_URL || ''
+
+  oreId = new OreId({ 
+    isUsingProxyServer: true, 
+    appId,
+    plugins:{popup: WebPopup()},
+    oreIdUrl,
+  });
+  await oreId.init()
+
+  oreId.popup.auth({ provider: 'google' as AuthProvider}).then(async (response) => {
+    console.log(response)
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
+const runTxn = async () => {
+  if(!oreId) {
+    console.log('login first')
+    return
+  }
+
+  const userChainAccounts = oreId.auth.user.data.chainAccounts;
+  const ethAccount = userChainAccounts.find(ca => ca.chainNetwork === 'eth_goerli')
+
+  const transactionBody = {
+    from: ethAccount?.chainAccount!,
+    to: "0x60d5DA4FC785Dd1dA9c2dAF084B2D5ba478c8f8b",
+    value: "0x01"
+  }
+
+  try {
+    const transaction = await oreId.createTransaction({
+      transaction: transactionBody,
+      chainAccount: ethAccount?.chainAccount,
+      chainNetwork: ethAccount?.chainNetwork!,
+    })
+    console.log(transaction)
+    const { transactionId } = await oreId.popup.sign({ transaction })
+    console.log(transactionId)
+  } catch (error) {
+    console.log(error)
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={login}>Login</button>
+        <button onClick={runTxn}>Run Transaction</button>
       </header>
     </div>
   );
